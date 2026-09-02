e24be849f1cd12966687b483ea162e0de18c7282
Nigel Ng <ngnigel33@gmail.com>
2026-09-02 12:47:03 +0800

    Change vcm_inference to vcm_prediction

--- panelmodels/__init__.py
+++ panelmodels/__init__.py
@@ -1,8 +1,8 @@
 from panelmodels import panelmodels
 from panelmodels import utils
 
-from panelmodels.panelmodels import (VariableCoefficientModel, vcm_inference,)
+from panelmodels.panelmodels import (VariableCoefficientModel, vcm_prediction,)
 from panelmodels.utils import (add_lag_features,)
 
 __all__ = ['VariableCoefficientModel', 'add_lag_features', 'panelmodels',
-           'utils', 'vcm_inference']
+           'utils', 'vcm_prediction']
