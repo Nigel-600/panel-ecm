4708f81ecfa6862105e674ead6dbeaaa220d4467
Nigel Ng <ngnigel33@gmail.com>
2026-09-04 16:42:34 +0800

    update panelmodels/__init__.py

--- panelmodels/__init__.py
+++ panelmodels/__init__.py
@@ -1,8 +1,9 @@
 from panelmodels import panelmodels
 from panelmodels import utils
 
-from panelmodels.panelmodels import (VariableCoefficientModel, vcm_prediction,)
+from panelmodels.panelmodels import (VariableCoefficientModel, sur_prediction,
+                                     swamy_prediction,)
 from panelmodels.utils import (add_lag_features,)
 
 __all__ = ['VariableCoefficientModel', 'add_lag_features', 'panelmodels',
-           'utils', 'vcm_prediction']
+           'sur_prediction', 'swamy_prediction', 'utils']
